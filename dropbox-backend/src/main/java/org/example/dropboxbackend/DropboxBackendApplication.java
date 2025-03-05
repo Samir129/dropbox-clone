2cf93d164a2feedc1478e74af69c885e6364956f
Samir <ms921849@student.nitw.ac.in>
2025-03-06 01:28:31 +0530

    first-commit

---
+++ dropbox-backend/src/main/java/org/example/dropboxbackend/DropboxBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.dropboxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropboxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DropboxBackendApplication.class, args);
+    }
+
+}
---
+++ dropbox-backend/src/test/java/org/example/dropboxbackend/DropboxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dropboxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropboxBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
